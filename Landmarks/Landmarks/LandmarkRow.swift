aa5cc97ebc89fde5cba26598d98b3eb425852eb1
Adam Chehadi <adampchehadi@gmail.com>
2019-12-22 12:36:17 -0700

    Building Lists and Navigation

---
+++ Landmarks/Landmarks/LandmarkRow.swift
@@ -0,0 +1,35 @@
+//
+//  LandmarkRow.swift
+//  Landmarks
+//
+//  Created by Adam Chehadi on 12/21/19.
+//  Copyright © 2019 Adam Chehadi. All rights reserved.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    let landmark: Landmark
+    
+    var body: some View {
+        HStack {
+            landmark.image
+                .resizable()
+                .frame(width: 50, height: 50)
+            
+            Text(landmark.name)
+            
+            Spacer()
+        }
+    }
+}
+
+struct LandmarkRow_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            LandmarkRow(landmark: landmarkData[0])
+            LandmarkRow(landmark: landmarkData[1])
+        }
+        .previewLayout(.fixed(width: 300, height: 70))
+    }
+}
